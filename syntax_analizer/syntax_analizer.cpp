2b916d177c52c1867124b7c5722c1fe4fa92019c
ysh4296 <dbtmdgns4296@naver.com>
2021-06-05 15:25:13 +0900

    Update syntax_analizer.cpp

--- syntax_analizer/syntax_analizer.cpp
+++ syntax_analizer/syntax_analizer.cpp
@@ -19,11 +19,11 @@ struct token {// struct for token
 	int type;// type of token for input of S_R_Table
 	ii loc;// location of token
 }; 
-string S_R_table[86][40];// S_R table for shift-reduce operation
-stack<int> st;// stack for save current state
+string S_R_table[87][41];// S_R table for shift-reduce operation
+stack<int> local;// stack for save current state
 vector<token> token_list;// out of lexical analizer and input of syntax analizer
 vector<ci> trans_table;// table for translate tokens to push in token_list
-ii Reduce_Rule[38];// reduce rule for reduce operation [reduced_type number]  # for erase at this reduce }
+ii Reduce_Rule[39];// reduce rule for reduce operation [reduced_type number]  # for erase at this reduce }
 int cur_state = 1;// the start state is 0
 int index = 0; //index for current token position
 void S_R_table_in() // Fill in the Shift_Reduce table from "S_R_table.txt" which is in same directory
@@ -32,12 +32,19 @@ void S_R_table_in() // Fill in the Shift_Reduce table from "S_R_table.txt" which
     is.open("S_R_Table.txt");
 	string st;
 	int r, c;
+	for (int r = 0; r < 87; r++) {
+		for (int c = 0; c < 41; c++) {
+			S_R_table[r][c] = "x";
+		}
+	}
+	cout << "sr_in" << endl;
 	if (is.is_open()) {
 		while (!is.eof()) {
 			is >> r >> c >>st;
 			S_R_table[r][c] = st;// fill the table
 		}
 	}
+	cout << "sr_out" << endl;
 	is.close();
 }
 void Type_Trans_table_in() // fill trans table for use type in type_trans_table
@@ -72,8 +79,7 @@ void Reduce_Rule_in() // fill reduce rules for reduce
 	int cnt = 0;
 	if (is.is_open()) {
 		while (!is.eof()) {
-			is >> F;
-			is >> S;
+			is >> F >> S;
 			Reduce_Rule[cnt] = { F , S };// fill the table
 			cnt++;
 		}
@@ -91,33 +97,26 @@ void token_in(string buf) {// get the input file in token vector to parse
 	vector<char*> list;
 	T = strtok(S, ">");
 	while (T != NULL) {
-		cout << T << endl;
 		list.push_back(T);
 		T = strtok(NULL, ">");
 	}
-	for (int i = 0 ; i < (int)list.size() ; i++) {
-		cout << "list   " << list[i] << endl;
+	for (int i = 0 ; i < (int)list.size() - 1 ; i++) {
 		if (list[i][0] == '\n') { // next token is in next line
-			cout << "n" << endl;
 			line++;
 			comp = 1;
 			T = strtok(list[i], "<");
-			T = strtok(NULL, ",");
-			cout << "token : " << T << endl;
+			T = strtok(NULL, ":");
 			f = T;
-			T = strtok(NULL, ",");
+			T = strtok(NULL, ":");
 			s = T;
-			cout << s << " "<< f << endl;
 			token_list.push_back(token(s, Type_Trans(f), { line,comp }));
 			comp++;
 		} else { // next token is in the same line
-			cout << "0" << endl;
 			T = strtok(list[i], "<");
-			T = strtok(T, ",");
+			T = strtok(T, ":");
 			f = T;
-			T = strtok(NULL, ",");
+			T = strtok(NULL, ":");
 			s = T;
-			cout << s << " " << f << endl << endl;
 			token_list.push_back(token(s, Type_Trans(f), {line,comp}));
 			comp++;
 		}
@@ -126,46 +125,54 @@ void token_in(string buf) {// get the input file in token vector to parse
 bool Reduce(token cur,string cmd) {
 	int next = stoi(cmd.substr(1));
 	int erase = Reduce_Rule[next].second;
+	cout << "next   " << next << "    erase  " << erase << endl;
 	while (erase--) {
-		st.pop();
+		local.pop();
 	}
-	int cu = st.top();
-	next = stoi(S_R_table[cu][next]);
-	if (next == 0) return false;
-	st.push(next);
+	int cu = local.top();
+	if (S_R_table[cu][Reduce_Rule[next].first][0] == 'x') return false;
+	next = stoi(S_R_table[cu][Reduce_Rule[next].first]);
+	cout << "nexT_top" << next << endl;
+	local.push(next);
 	return true;
 }
-bool Shift(token cur,string cmd) {
+void Shift(token cur,string cmd) {
 	int next = stoi(cmd.substr(1));
-	if (next == 0) return false;
-	st.push(next);
+	local.push(next);
 	index++;
-	return true;
+	return;
 }
 bool solve() {
-	st.push(1);
-	while (st.top() != -1) { //  the acc is -1
+	local.push(0);
+	cout << "solve_in" << endl;
+	cout << "solve_stack push" << endl;
+	while (local.top() != -1) { //  the acc is -1
+		cout << token_list[index].data  << "     stack_top ()   " << local.top()<< endl;
 		token cur = token_list[index];
-		string cmd = S_R_table[st.top()][cur.type];
+		cout << local.top() << " check " << cur.type << endl;
+		string cmd = S_R_table[local.top()][cur.type];
+		cout << "cmd   : " << cmd << endl;
 		switch(cmd[0]) {
 			case 's':
-				if (Shift(cur, cmd)) return false;
+				cout << "Shift" << endl;
+				Shift(cur, cmd);
 				break;
 			case 'r':
-				if (Reduce(cur, cmd)) return false;
+				cout << "Reduce" << endl;
+				if (!Reduce(cur, cmd)) return false;
 				break;
+			case 'a':
+				return true;
 			default:
 				return false;
 				break;
 		}
 	}
-	return true;
 }
 int main(int argc, char* argv[]) {
-	cout << "hello " << endl;
 	S_R_table_in(); //init S_R_Table
 	Type_Trans_table_in(); // init trans table
-	//Reduce_Rule_in(); // init Reduce_table
+	Reduce_Rule_in(); // init Reduce_table
 	// get the out of lexical analyzer as input token
 	ifstream readFile;
 	readFile.open(argv[1]);
@@ -174,9 +181,10 @@ int main(int argc, char* argv[]) {
 	char* end = new char[4]; 
 	strcpy(end,"end");
 	token_list.push_back(token(end, 21, { 0,0 })); // add ""
+	cout << "token_set end" << endl;
 	if (solve())
 		cout << "accept";
 	else
-		cout << "syntax_error : there can't be token named ' " << token_list[index].data << " ' in " << token_list[index].loc.first <<"th token of line number" << token_list[index].loc.second;
+		cout << "syntax_error : there can't be token named ' " << token_list[index].data << " ' in " << token_list[index].loc.second <<"th token of line number" << token_list[index].loc.first;
 	return 0;
 }
